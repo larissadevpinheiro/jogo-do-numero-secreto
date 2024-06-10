6bf030a0c7938cf7b561b68579292fb6f5e69c1e
larissadevpinheiro <larissa.pinheiro@kaizenconsultoria.net>
2024-06-10 16:24:40 -0300

    Update app.js
    Correção palavra tentativa

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentaviva';
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
